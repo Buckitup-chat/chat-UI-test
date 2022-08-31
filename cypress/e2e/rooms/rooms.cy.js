b033a63a4bc35b9fb6b24994c380b70437208a06
Ruslan Kurbanov <ruslankurbanov@MacBook-Pro-lits-admin.local>
2022-08-31 15:46:59 +0300

    classes edited

--- cypress/e2e/rooms/rooms.cy.js
+++ cypress/e2e/rooms/rooms.cy.js
@@ -2,9 +2,9 @@ describe('rooms', () => {
     before(()=> {
         const userName = 'Cypres test user';
         cy.visit(Cypress.env('url'));
-        cy.get('.t-form')
+        cy.get('.t-login-form')
         .find('[type="text"]').type(userName,{force:true});
-        cy.get('.t-form').submit();
+        cy.get('.t-login-form').submit();
         cy.get('.t-my-notes').click();
         cy.get('.t-chat-header').find('.t-peer-name').should('have.text', userName);
         cy.get('.t-rooms').click();
