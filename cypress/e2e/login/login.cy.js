879bb78a73eda1fc9706463f3f9c2c788b9dcb97
Ruslan Kurbanov <ruslankurbanov@MacBook-Pro-lits-admin.local>
2022-08-31 15:54:25 +0300

    login tests refactoring

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,20 +1,18 @@
-const path = require("path");
 describe('login a user', () => {
   const userName = 'Cypres test user';
   it('successfully logined', () => {
-    cy.visit(Cypress.env('url'))
-    cy.get('.t-form')
-    .find('[type="text"]').type(userName,{force:true})
-    cy.get('.t-form').submit()
+    cy.visit(Cypress.env('url'));
+    cy.get('.t-login-form')
+    .find('[type="text"]').type(userName,{force:true});
+    cy.get('.t-login-form').submit();
+    cy.get('.t-my-notes').click();
+    cy.get('.t-chat-header').find('.t-peer-name').should('have.text', userName);
   })
-  it("check if it's mine login", () => {
-    cy.get('.t-my-notes').click()
-    cy.get('.t-chat-header').find('.t-peer-name').should('have.text', userName)
-  })
-  it('Successfully login via import the recovery keys  without password', () => {
-    cy.get('.row > .sidebarIcon').click();
-    cy.get('#logout-backup-popup-content > .mt-5').click();
-     cy.get('.mt-3.h-12').click(); 
-     cy.get('.t-exit').click()    
-  })
+
+  // it('Successfully login via import the recovery keys  without password', () => {
+  //   cy.get('.row > .sidebarIcon').click();
+  //   cy.get('#logout-backup-popup-content > .mt-5').click();
+  //    cy.get('.mt-3.h-12').click(); 
+  //    cy.get('.t-exit').click()    
+  // })
 })
