1fc25bbc54ac5111d2ccdb48bf2d74b8b9066777
Ruslan Kurbanov <ruslankurbanov@MacBook-Pro-lits-admin.local>
2022-08-31 15:55:47 +0300

    unnecessary code deleted

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -8,11 +8,4 @@ describe('login a user', () => {
     cy.get('.t-my-notes').click();
     cy.get('.t-chat-header').find('.t-peer-name').should('have.text', userName);
   })
-
-  // it('Successfully login via import the recovery keys  without password', () => {
-  //   cy.get('.row > .sidebarIcon').click();
-  //   cy.get('#logout-backup-popup-content > .mt-5').click();
-  //    cy.get('.mt-3.h-12').click(); 
-  //    cy.get('.t-exit').click()    
-  // })
 })
